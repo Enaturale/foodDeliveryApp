73595fe54bdf6eec5f26a47e3cd8b059a90bc5ab
Enaturale <bukolaarowosebi@gmail.com>
2022-06-02 17:16:55 +0100

    screens and component files

---
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, Text } from "react-native";
+import Styles from "./Styles";
+
+
+const HomeScreen = ({navigation}) => {
+    
+    return(
+        <View style={Styles.container}>
+            <Text>Home Screen</Text>
+        </View>
+    )
+}
+
+export default HomeScreen;
+
